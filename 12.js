517a2a5e7d801c228069287d46689a51eb907406
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-29 19:07:54 +0530

    booleans(T , F) and comparision operators(>, >=, <, <=, ==, ===, !=, !==)

---
+++ 12.js
@@ -0,0 +1,21 @@
+// booleans & comparison operators
+
+let num1 = 7;
+let num2 = 7;
+
+// > , < , >=, <= : Comparison Operators
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 < num2);
+
+// == vs ===
+// loose equality(==)
+// console.log(num1 == num2);// only checks for value// doesnot checks for dataType
+// strict equality(===)
+// console.log(num1 === num2);// strictly checks for both values and dataTypes
+
+// != vs !==
+// loose Inequality vs Strict Inequality
+
+// console.log(num1 != num2);
+// console.log(num1 !== num2);
