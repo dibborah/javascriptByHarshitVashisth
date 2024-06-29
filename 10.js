c3f0cbf81f0f2a31b0ead2535d422968c9cbdc37
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-29 17:46:48 +0530

    Completed template strings

--- 10.js
+++ 10.js
@@ -1,1 +1,13 @@
-console.log('hello universe2!!!');
+// template string 
+
+// here we use backticks (``) , dollar($) and placeholders (curly braces)({})
+
+let age = 22;
+let firstName = 'harshit';
+
+// we want
+// 'my name is harshit and my age is 22'
+
+// let aboutMe = 'my name is ' + firstName + ' and my age is ' + age;
+let aboutMe = `my name is ${firstName} and my age is ${age}`;
+console.log(aboutMe);
