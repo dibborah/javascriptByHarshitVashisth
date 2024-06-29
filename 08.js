e968b823bd399bdd2baf3ed7c06ced967046bbaf
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-29 16:52:07 +0530

    data types in js and converting a string to number and vice versa

--- 08.js
+++ 08.js
@@ -1,1 +1,56 @@
-console.log('hello universe2!!!');
+// typeof operator 
+
+// data types in js
+// Two types: a. Primitive b. Referenced
+// a. Primitives(7) : 
+// number
+// string
+// booleans
+// null
+//undefined
+// BigInt
+// symbol
+
+// b. Referenced: 
+// a. Functions
+// b. Objects
+
+let age = 6;
+let aString = "top";
+let numberString = "5";
+
+// console.log(typeof age)
+// console.log(typeof aString)
+// console.log(typeof 0)
+// console.log(typeof 0.8)
+// console.log(typeof -8.3)
+// console.log(typeof '')
+// console.log(typeof 'hello')
+// console.log(typeof '6')
+
+// convert number to string
+
+// 2 ways to convert: 
+// 1. Add or concatenate with empty string('')
+// 2. use String() method
+
+
+// console.log(typeof (age + ''));
+// age = age + '';
+// console.log(typeof age);
+
+// console.log(typeof age);
+// console.log(typeof String(age));
+
+// convert string to number
+
+// 2 ways to convert:
+// 1. Write a plus(+) sign just before the variable or value without any gaps
+// 2. use the Number() method
+
+// console.log( typeof +aString);
+
+// console.log(typeof aString)
+// console.log(typeof Number(aString))
+// console.log(typeof numberString)
+// console.log(typeof Number(numberString))
