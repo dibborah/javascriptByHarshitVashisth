83a1d57c6a99042f1e01237dba1a4eee7f558efe
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-29 19:40:34 +0530

    ternary operator

---
+++ 15.js
@@ -0,0 +1,19 @@
+// ternary operator
+
+// let age = 8;
+// let drink;
+
+// if(age >= 5) {
+//     drink = "coffee";
+// }else{
+//     drink="milk";
+// }
+
+// console.log(drink);
+
+// ternary operator (condition ? (if conditon true) : (else))
+// drink = age >= 5 ? "coffee" : "milk";
+// console.log(drink);
+
+
+
