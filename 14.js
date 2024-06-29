12dd776f3c11cfdac22fe674f7bd9aae91321c70
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-29 19:35:35 +0530

    truthy values and falsy values and using them in conditional statements

---
+++ 14.js
@@ -0,0 +1,44 @@
+// if else condition
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log('User can play ddlc');
+// }else{
+//     console.log('User can play mario')
+// }
+
+// let num = 13;
+
+// console.log(12 % 5);// Modulo operator is used to find remainder
+
+// if(num % 2 === 0){
+//     console.log('number is even');
+// }else{
+//     console.log('number is odd');
+// }
+
+
+// false
+// null
+// undefined
+// "",
+// 0
+// NaN
+
+// let firstName = "";
+// let firstName = null;
+// let firstName = undefined;
+let firstName;
+// let firstName = false;
+// let firstName = 0;
+// let firstName = NaN;
+// let firstName =  0 /  0;
+// let firstName =  "harshit" /  6;
+// console.log(firstName);
+
+if(firstName) {
+    console.log(firstName);
+}else{
+    console.log('firstName is empty')
+}
