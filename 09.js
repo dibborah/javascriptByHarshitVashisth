e5fc33c2c80fb483680238ecb498c822500fbb90
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-29 17:39:25 +0530

    completed string concatenation

--- 09.js
+++ 09.js
@@ -1,1 +1,11 @@
-console.log('hello universe2!!!');
+// string concatenation
+
+// let string1 = 'harshit';
+// let string2 = 'vashistha';
+let string1 = '17';
+let string2 = '10';
+
+// let fullName = string1 + string2;
+// let fullName = string1 + " " +  string2;
+let fullName = +string1 + +string2;
+console.log(typeof fullName);
