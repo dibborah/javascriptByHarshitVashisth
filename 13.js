962bddb7932d55bf41b2c12cbe44402de7d8ed29
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-29 19:20:57 +0530

    truthy and falsy values

---
+++ 13.js
@@ -0,0 +1,21 @@
+// truthy and falsy values
+
+// falsy values
+
+// false
+// null
+// undefined
+// "",
+// 0
+// NaN
+
+// Rest everthing are truthy values(1, -1.3, 0.1, -0.1, "abc", etc);
+
+let num =  -1.32;
+// let num =  -0.1;
+
+if(num){
+    console.log(num, 'a truthy value');
+}else{
+    console.log('num is a falsy value')
+}
