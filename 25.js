7fd3e9faa547af71077a8b869c903f2d22c97982
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-30 20:11:09 +0530

    done do while loop

---
+++ 25.js
@@ -0,0 +1,22 @@
+// do while loop
+
+// do while loop pehli bar to chalega hi chalega chahe condition false ho unlike other loops
+// Phir condition check hogi(ek baar chalne ke baad)
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i > 9);
+// console.log('i current value', i);
+
+// comparing do while with while loop
+
+// let i = 10;
+
+// while (i <= 9) {
+//     console.log(i);
+//     i++;
+// }
+
+// console.log('i current value', i);
