38c1e88582c6c8887c3d6fb5964bb9511b2c483b
dibya.borah <dibyajyotiborah469@gmail.com>
2024-07-01 00:26:25 +0530

    array methods: push pop unshift shift

---
+++ 27.js
@@ -0,0 +1,40 @@
+// array methods :
+
+// push pop
+// shift unshift
+
+
+// push()
+
+// let fruits = ['mango', 'papaya', 'orange'];
+// console.log(fruits);
+// fruits.push('banana');
+// console.log(fruits);
+
+// pop()
+
+// let fruits = ['mango', 'papaya', 'orange'];
+// console.log(fruits);
+// const poppedFruit = fruits.pop();// pop() method also returns the popped item along w/ removing it
+// console.log(poppedFruit);
+// console.log(fruits);
+
+// unshift()
+
+// let fruits = ['mango', 'papaya', 'orange'];
+
+// console.log(fruits);
+// fruits.unshift('banana');
+// console.log(fruits);
+
+
+// shift()
+// let fruits = ['mango', 'papaya', 'orange'];
+
+// console.log(fruits);
+// const shiftedFruit = fruits.shift()
+// console.log(shiftedFruit);
+// console.log(fruits);
+
+
+
