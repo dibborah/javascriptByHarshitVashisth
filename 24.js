0fc9a62f2a756f378b5aaec3fc8aa5c07720dfa8
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-30 20:01:24 +0530

    Done break and continue keyword

---
+++ 24.js
@@ -0,0 +1,13 @@
+// break keyword
+
+// continue keyword
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 4) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
+
+console.log('Hello world');
