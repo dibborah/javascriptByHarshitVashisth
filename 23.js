cace8f3c910798a38bf3ab2974cda5c8dd6a5957
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-30 19:45:56 +0530

    completed for loop example

---
+++ 23.js
@@ -0,0 +1,11 @@
+// for loop example
+// print the sum of first 10 natural numbers
+
+let i = 0;
+let totalSum = 0;
+const num = 100;
+for (; i <= num; i++) {
+    totalSum = totalSum + i;
+}
+
+console.log(totalSum);
