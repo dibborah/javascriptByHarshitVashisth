bda9d0b5dd4ed11eb7d06593b2a921aabe8f775d
dibya.borah <dibyajyotiborah469@gmail.com>
2024-06-30 19:29:10 +0530

    while loop using example proble

---
+++ 21.js
@@ -0,0 +1,20 @@
+// Sum of first n natural numbers
+// using whlile formula
+
+// let totalSum = 0;
+// let i = 0;
+
+// while(i <= 100){
+//     totalSum += i;
+//     // totalSum = totalSum + i;
+//     i++;
+// }
+
+// console.log('totalSum', totalSum);
+
+// Sum of first n natural numbers
+// using Maths formula
+let n = 10;
+let totalSum = (n * (n + 1)) / 2;
+
+console.log('using maths formula', totalSum);
