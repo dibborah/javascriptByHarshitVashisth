d03f3d8f7baaeb5e749787d3d3fc6bf95ba766b4
dibya.borah <dibyajyotiborah469@gmail.com>
2024-07-01 19:55:19 +0530

    done using const to declare array(reference data types)

---
+++ 31.js
@@ -0,0 +1,15 @@
+// use const for creating array
+
+// array is stored in heap memory
+// the reference(address) of the array is stored in stack(ex: 0x11)
+
+const fruits = ['mango', 'papaya'];
+// fruits = ['chiku', 'grapes'];
+
+fruits.push('banana');
+console.log(fruits);
+
+// Note: Use const to declare variables when declaring reference data types
+
+
+
