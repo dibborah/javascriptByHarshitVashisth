3ff0bb3ca4cc9d690d09e79fff4675fc10a33135
dibya.borah <dibyajyotiborah469@gmail.com>
2024-07-01 20:06:22 +0530

    done for of loop in array

---
+++ 33.js
@@ -0,0 +1,11 @@
+// for of loop in array
+
+const fruits = ['mango', 'papaya', 'grapes'];
+
+const newArray = [];
+for (const fruit of fruits){
+    newArray.push(fruit.toUpperCase());
+};
+
+console.log(newArray);
+
