523e7a57454f43e24897fba58302b3f80edfd14f
dibya.borah <dibyajyotiborah469@gmail.com>
2024-07-01 20:15:08 +0530

    done for of and traditional for loop to iterate an array

--- 33.js
+++ 33.js
@@ -2,10 +2,18 @@
 
 const fruits = ['mango', 'papaya', 'grapes'];
 
+// const newArray = [];
+// new for of loop to iterate an array
+// for (const fruit of fruits){
+//     newArray.push(fruit.toUpperCase());
+// };
+
+// console.log(newArray);
+
 const newArray = [];
-for (const fruit of fruits){
-    newArray.push(fruit.toUpperCase());
-};
-
+// Traditional for loop to iterate an array
+for (let i = 0; i < fruits.length; i++ ){
+    // console.log(fruits[i].toUpperCase());
+    newArray.push(fruits[i].toUpperCase());
+}
 console.log(newArray);
-
