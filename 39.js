b616674f7122dbdfd4a4be814cdda04d61d9e140
dibya.borah <dibyajyotiborah469@gmail.com>
2024-07-03 11:58:41 +0530

    done computed properties

---
+++ 39.js
@@ -0,0 +1,21 @@
+// computed properties
+
+const key1 = 'objkey1';
+const key2 = 'objkey2';
+
+const value1 = 'myValue1';
+const value2 = 'myValue2';
+
+// 1st solution
+// const obj = {
+//     [key1] : value1,
+//     [key2] : value2,
+// };
+
+// 2nd solution
+// const obj = {};
+// obj[key1] = value1;
+// obj[key2] = value2;
+
+
+console.log(obj)
