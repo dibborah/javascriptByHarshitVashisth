6fca67e92dd7d74ba5cb8eaea6583cbe39a26381
dibya.borah <dibyajyotiborah469@gmail.com>
2024-07-12 01:52:26 +0530

    done hoisting intro

---
+++ 47.js
@@ -0,0 +1,27 @@
+// hoisting
+
+// hello();
+
+// only possible to call before initializing in case of function declaration
+// function hello() {
+//     console.log('hello world');
+// }
+
+// using arrow function
+// var hello = () => {
+//     console.log('hello world');
+// }
+
+// using function expression
+// let hello = function() {
+//     console.log('hello world');
+// }
+
+// hello();
+
+console.log(firstName);
+
+// let firstName = 'harshit';
+var firstName = 'harshit';
+
+console.log(firstName);
