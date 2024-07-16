0c94318cee5845827e6f5c7e6cb78e7c8a06d755
dibya.borah <dibyajyotiborah469@gmail.com>
2024-07-16 10:04:50 +0530

    done map method

---
+++ 57.js
@@ -0,0 +1,32 @@
+// map method
+
+const numbers = [3,4,5,1,8];
+
+// function SquareNumber(number) {
+//     // return number * number;
+//     console.log(number * number);
+// }
+
+// const squareArray = numbers.map(SquareNumber);
+// console.log(squareArray);
+
+// when not returning anything(undefined returns)
+// numbers.map(SquareNumber); // in map function if we return nothing "undefined" is returned by default
+
+// const squareArray = numbers.map((number, index) => {
+//     // return `index: ${index} and ${number * number}`;
+//     return index;
+// })
+// console.log(squareArray);
+
+const users = [
+    {firstName: 'harshit', age: 21},
+    {firstName: 'mohit', age: 23},
+    {firstName: 'rahul', age: 26},
+];
+
+const userName = users.map(user => user.firstName);
+console.log(userName);
+
+
+
