327b664652a1c2c6d2fc2d1ea901e8165e67e069
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-03 17:08:28 +0530

    done more about prototype

---
+++ 85.js
@@ -0,0 +1,46 @@
+// more about prototype
+
+// let numbers = [1, 2, 3];
+
+// const numbers = new Array(1,2,3);
+// console.log(Array.prototype);
+// console.log(Array.isArray(Object.getPrototypeOf(numbers)));
+// console.log(Array.isArray(numbers.__proto__));
+
+
+// js main Array v object hote hain
+// console.log((Array.prototype));
+
+// function hello() {
+//     console.log('hello world');
+// }
+
+// console.log(hello.prototype);
+// hello.prototype = [];
+// console.log(hello.prototype);
+// hello.prototype.push(1);
+// console.log(hello.prototype);
+// console.log(Array.isArray(hello.prototype));
+
+
+// ----- my own practice for further understanding of concepts -----
+// function hello() {
+//     console.log('hello world');
+// }
+
+// const obj = {
+//     firstName: 'harshit',
+// }
+
+// // hello.prototype.lastName = 'vashistha';
+
+// obj.__proto__ = hello.prototype;
+
+// // console.log('hello', hello.prototype);
+
+// obj.__proto__.lastName = 'vashistha';
+// console.log('obj', obj.__proto__);
+// console.log(obj.lastName);
+
+
+
